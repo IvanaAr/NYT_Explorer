a0e4b516b4e7078a75dbec82cf05ab9ac039daca
ivananutrition <ivana.arizanovic@hotmail.com>
2018-02-27 14:30:53 +0100

    added second API responce

--- js/NYT_Explorer.js
+++ js/NYT_Explorer.js
@@ -1,6 +1,9 @@
+
+
 class App extends React.Component {
   constructor(props) {
       super(props)
+      
       this.state = {
           articles: [],
       }
@@ -13,18 +16,24 @@ class App extends React.Component {
       // let articles=responseData.response.docs.slice(0,20)
       // this.setState({ articles: articles });
       // console.log(articles[0].web_url)
-       let articles = [];
-          for(var i = 0; i < 20; i++) {
-            const doc = responseData.response.docs[i];
-            articles.push(doc.web_url);
-          }
-          this.setState({'articles': articles});
 
+       let articles = [];
+          for(var i = 0; i < 1; i++) {
+            const article = responseData.response.docs[i];
+            articles.push(article.web_url);
+            console.log(article.web_url)
+          }
+          this.setState({articles: articles});
+          console.log(articles)
+          console.log("hello from first api")
+         
         }
 
   componentDidMount() {
+    const year=document.getElementById('date').value.slice(0,4);
+    const month=document.getElementById('date').value.slice(5,7).replace(/^0+/,'');
       $.ajax({
-          url: 'https://api.nytimes.com/svc/archive/v1/1999/2.json',
+           url: 'https://api.nytimes.com/svc/archive/v1/'+year+'/'+month+'.json',
           method: 'GET',
           data: {
               'api-key': "e5116ab98a934e51954e8ba2bb9e0edb"
@@ -36,22 +45,64 @@ class App extends React.Component {
     if (!this.state.articles || this.state.articles.length == 0) {
       return 'Nothing to display';
     }
-    return (<Item url={this.state.articles} />)
-          
+    return(
+      <div id="articles"> 
+        {
+          this.state.articles.map(
+            (article, index) => <Item key={index} article={article}/>
+          )
+        }</div>
+        )
   }
 
 }
 
 
 class Item extends React.Component {
+  
+  constructor(props){
+  super(props)
+  this.state={preview:{}
+  }
+
+this.done=this.done.bind(this);
+}
+
+done(results){
+  this.setState({ preview: results });
+  console.log(results)
+  console.log("helo from second api")
+
+    }
+   
+
+    componentDidMount() {
+    const url='http://api.linkpreview.net/?key=5a901d221b994d82ced4d399f271c1f206b76f8e6a893&q='+this.props.article
+      $.ajax({
+          url: url,
+          method: 'GET',
+          success: this.done
+         }
+      );
+  }
+  
+
     render() {
         return ( 
           <div>
-              <a href = { this.props.url } target = "_blank" > { this.props.url } </a> 
+              <p>{this.state.articles} </p> 
+              <p>{this.state.preview.title}</p>
+              <img src={this.state.preview.image}/>
+              <p>{this.state.preview.description}</p>
+              <p>{this.state.preview.url}</p> 
           </div>
         );
     }
 }
+
+
+
+
 
 
 function search() {
@@ -62,12 +113,3 @@ function search() {
 
 }
   
-
-
-
-
-
-
-
-
-
