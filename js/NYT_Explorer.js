6056bc0a13a76f699bb24210f65e0c587fa502a6
ivananutrition <ivana.arizanovic@hotmail.com>
2018-03-02 20:58:07 +0100

    added inicial css

--- js/NYT_Explorer.js
+++ js/NYT_Explorer.js
@@ -10,9 +10,8 @@ class App extends React.Component {
 
 
   setData(responseData) {
-    
-       let articles = [];
-          for(var i = 0; i < 1; i++) {
+    let articles = [];
+          for(var i = 0; i < 2; i++) {
             const article = responseData.response.docs[i];
             articles.push(article);
             console.log(article)
@@ -37,13 +36,13 @@ class App extends React.Component {
   }
   render() {
     if (!this.state.articles || this.state.articles.length == 0) {
-      return 'Nothing to display';
+      return "";
     }
     return(
       <div id="articles"> 
         {
           this.state.articles.map(
-            (article, index) => <Item key={index} article={article}  id={index} />
+            (article, index) => <Item key={index} article={article} id={index}/>
           )
         }</div>
         )
@@ -67,25 +66,19 @@ class Item extends React.Component {
   }
 this.clickArticle=this.clickArticle.bind(this);
 this.done=this.done.bind(this);
+
 }
 clickArticle(){
-  
-  
+
   console.log("hello from clickArticle")
-
-   
-   document.getElementById(this.props.id).style.display = "block";
-    
-
+  document.getElementById(this.props.id).style.display = "block";
 }
-
 done(results){
   this.setState({ preview: results });
   console.log(results)
   console.log("helo from second api")
 
-    }
-   
+   }
 
     componentDidMount() {
     const url='http://api.linkpreview.net/?key=5a901d221b994d82ced4d399f271c1f206b76f8e6a893&q='+this.props.article.web_url
@@ -98,25 +91,45 @@ done(results){
   }
   
 
-    render() {
+      render() {
         return ( 
-          <div>
-               
-              <p>{this.state.preview.title}</p>
-              <img width="120" src={this.state.preview.image} onClick={(e) => this.clickArticle(e)}/>
-              <p>{this.state.preview.description}</p>
-              <div id={this.props.id} style={{display:'none'}} >
-  {'Article Details'+JSON.stringify(this.state.selectedArticle,null,3)}
+
+          <div class="flexbox">
+       <div class="flex-item">
+              <p id="title">{this.state.preview.title}</p>
+             <p> <img id="imgpreview"  src={this.state.preview.image} onClick={(e) => this.clickArticle(e)}/></p>
+              <p id="description ">{this.state.preview.description}</p>
+              </div>
+              <div id="articleDetails" id={this.props.id} style={{display:'none'}} >
+  {'Article Details:'+ JSON.stringify(this.state.selectedArticle, null, 3)}
 </div>
-          </div>
+</div>
         );
-    }
+
+      }
 }
 
 
 
 
+
+
 function search() {
+
+    var elem = document.getElementById("myBar"); 
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100 ) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = width * 1 + '%';
+        
+    }
+
+    }
 
 
     const root = document.getElementById('root');
